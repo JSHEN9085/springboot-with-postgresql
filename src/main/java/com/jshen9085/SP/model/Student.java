16922bbfe7fb427e7df2418730fd52a109ab1177
JSHEN9085 <jshen9085@gmail.com>
2021-09-19 23:43:29 -0400

    first commit

---
+++ src/main/java/com/jshen9085/SP/model/Student.java
@@ -0,0 +1,52 @@
+package com.jshen9085.SP.model;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String name;
+    private LocalDate dob;
+    private Integer age;
+    private String email;
+
+    public Student(Long id, String name, LocalDate dob, Integer age, String email) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
---
+++ src/test/java/com/jshen9085/SP/SpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jshen9085.SP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
