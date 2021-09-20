31cf20c0576287515f66b08b8a5594bc49c49137
JSHEN9085 <jshen9085@gmail.com>
2021-09-20 00:36:10 -0400

    DB setup

--- src/main/java/com/jshen9085/SP/controller/StudentController.java
+++ src/main/java/com/jshen9085/SP/controller/StudentController.java
@@ -1,18 +1,30 @@
 package com.jshen9085.SP.controller;
 
 
+import com.jshen9085.SP.model.Student;
 import com.jshen9085.SP.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/students")
 public class StudentController {
 
-    @Autowired
+
     private final StudentService studentService;
 
-    public
+    @Autowired
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudent(){
+        return studentService.getStudents();
+    }
 
 }
